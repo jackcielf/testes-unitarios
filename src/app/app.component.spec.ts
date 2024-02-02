ef35ece6612bcd3d08bb9393fdbfe7d896955bba
JakkiFx <jackcielfelix555@gmail.com>
2024-02-02 12:04:30 -0300

    Configuração do Jest funcionando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,27 +3,32 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+  );
 
-  it('should create the app', () => {
+  it('Deve criar o componente', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tests-units'`, () => {
+  it(`A variável 'title' deve ter seu valor 
+      exatamente igual a 'testes unitários'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tests-units');
+    expect(app.title).toEqual('testes unitários');
   });
 
-  it('should render title', () => {
+  it('Deve renderizar o titulo', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tests-units app is running!');
+
+    const compiled = fixture.nativeElement;
+    const titleText = compiled.querySelector('.title')?.textContent;
+    expect(titleText).toContain('testes unitários app está rodando!');
   });
 });
